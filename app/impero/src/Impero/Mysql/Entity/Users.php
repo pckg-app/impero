312ceea7c72c31d0f36bb05d94e0ca0f3569bcf7
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-11-07 02:22:58 +0100

    Relations

--- app/impero/src/Impero/Mysql/Entity/Users.php
+++ app/impero/src/Impero/Mysql/Entity/Users.php
@@ -1,7 +1,7 @@
 <?php namespace Impero\Mysql\Entity;
 
-use Impero\Mysql\Record\Database;
 use Impero\Mysql\Record\User;
+use Impero\Servers\Entity\Servers;
 use Pckg\Database\Entity;
 use Pckg\Maestro\Service\Contract\Entity as MaestroEntity;
 
@@ -20,6 +20,12 @@ class Users extends Entity implements MaestroEntity
     public function getAddUrl()
     {
         return url('user.add');
+    }
+
+    public function server()
+    {
+        return $this->belongsTo(Servers::class)
+                    ->foreignKey('server_id');
     }
 
 }
