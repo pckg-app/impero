e673db6b6206714c6299cb1632bc4a75ca79b279
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-01-31 17:23:14 +0100

    Fix

--- app/impero/src/Impero/Apache/Record/Site.php
+++ app/impero/src/Impero/Apache/Record/Site.php
@@ -420,7 +420,7 @@ class Site extends Record
             /**
              * We need to make sure that repository and branch are already checked-out on filesystem.
              */
-            $aliasDir = '/www/_linked/' . str_replace(['.', '@', '/'], '-', $pckg['repository']) . '/' .
+            $aliasDir = '/www/_linked/' . str_replace(['.', '@', '/', ':'], '-', $pckg['repository']) . '/' .
                         $pckg['branch'];
             $this->prepareLinkedCheckout($pckg, $aliasDir);
 
