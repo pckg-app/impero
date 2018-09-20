1fb2c59b161625c72ddce3f714e4699652029033
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-09-20 13:48:28 +0200

    Add consol

--- app/impero/src/Impero/Storage/Provider/Storage.php
+++ app/impero/src/Impero/Storage/Provider/Storage.php
@@ -1,5 +1,6 @@
 <?php namespace Impero\Storage\Provider;
 
+use Impero\Storage\Console\UploadToS3;
 use Pckg\Framework\Provider;
 
 class Storage extends Provider
@@ -8,7 +9,7 @@ class Storage extends Provider
     public function consoles()
     {
         return [
-
+            UploadToS3::class,
         ];
     }
 
